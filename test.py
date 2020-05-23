7f4bd9c0e13cc0e3e2096de36711ef877ae70ee4
Anshika-git-007 <64117029+Anshika-git-007@users.noreply.github.com>
2020-05-23 11:44:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 import unittest
 
-from betamax import Betamax
 from app import app
-
-with Betamax.configure() as config:
-    config.cassette_library_dir = 'test/fixtures'
-
-test_auth_token = 'MLth87eHvSAaCQ1vn7jTd0xA9Kapo5'
 
 
 class TestCases(unittest.TestCase):
 
-    def test_root_endpoint(self):
-        """Assert that the / endpoint correctly redirects to web app."""
-        response = app.test_client().get('/')
-        self.assertIn('login.uber.com', response.data)
+    def test_text(self):
+        """Assert that the click button works and returns text."""
+        app.debug = True
+		self.test_app = app.test_client()
+        response = app.test_client().get('/click')
+		self.assertIn('text_', response.data)
+        self.assertEquals(response.status_code, 200)
